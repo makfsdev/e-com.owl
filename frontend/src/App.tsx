0df4e129286e83cd4b369ec94b38927df832bcbb
makfsdev <makfsdev@gmail.com>
2023-07-28 15:24:23 +0100

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div>
+      <header>E-COM.OWL</header>
+      <main></main>
+      <footer>All right reserved</footer>
+    </div>
+  )
+}
+
+export default App
