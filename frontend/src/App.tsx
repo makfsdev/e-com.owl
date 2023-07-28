1b81e1ac6f31e7f9711ed47a686c0258f8d21874
makfsdev <makfsdev@gmail.com>
2023-07-28 16:06:57 +0100

    show products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,25 @@
+import { sampleProduct } from './data'
+import './App.css'
+
 const App = () => {
   return (
     <div>
       <header>E-COM.OWL</header>
-      <main></main>
+      <main>
+        <ul>
+          {sampleProduct.map((product) => (
+            <li key={product.slug}>
+              <img
+                className='product-image'
+                src={product.image}
+                alt={product.image}
+              />
+              <h2>{product.name}</h2>
+              <p>${product.price}</p>
+            </li>
+          ))}
+        </ul>
+      </main>
       <footer>All right reserved</footer>
     </div>
   )
